5ec61e164fea245ab01fe17b1fa6de90836c6130
Maxim Balashov <79164211428@yandex.ru>
2021-09-05 12:00:01 +0300

    Updates

---
+++ precache-manifest.47d5362d44f05385b7e2cc9543bd6b6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245bc5d1d9e378c4408982b243297998",
+    "url": "/shopping-list/index.html"
+  },
+  {
+    "revision": "7af090d368b710512b04",
+    "url": "/shopping-list/static/css/2.84b3ebfc.chunk.css"
+  },
+  {
+    "revision": "f9fc19d8084b20e2357d",
+    "url": "/shopping-list/static/css/main.fc2c3ec4.chunk.css"
+  },
+  {
+    "revision": "7af090d368b710512b04",
+    "url": "/shopping-list/static/js/2.90bc89f8.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/shopping-list/static/js/2.90bc89f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9fc19d8084b20e2357d",
+    "url": "/shopping-list/static/js/main.177b7ec6.chunk.js"
+  },
+  {
+    "revision": "083821c51c9b5f632b69",
+    "url": "/shopping-list/static/js/runtime-main.22440f91.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.103ea16c957f96c50fc5158f272d4374.js"
+  "/shopping-list/precache-manifest.47d5362d44f05385b7e2cc9543bd6b6e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/shopping-list/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
